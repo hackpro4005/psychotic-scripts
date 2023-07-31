543fae4c1be2a11d58d3bda6610992fc70fdc24a
hackpro4005 <114504302+hackpro4005@users.noreply.github.com>
2023-07-31 10:13:09 +0300

    Add files via upload

---
+++ naked woman.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+call webbrowser
+
+openhttps://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Rear-facing_naked_woman.jpg/800px-Rear-facing_naked_woman.jpg
---
+++ penis video.py
@@ -0,0 +1,3 @@
+import webbrowser
+
+open https://www.youtube.com/watch?v=JQz5iGgYETk&list=PLhYNeM90dJSOuDwymGheuSEZGH2u6E7zh&index=3&pp=iAQB
