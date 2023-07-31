82815d863d0b3bb17eb52e2f05e61f4f672bd3d3
hackpro4005 <114504302+hackpro4005@users.noreply.github.com>
2023-07-31 18:13:59 +0300

    fixed penis video.py

--- penis video.py
+++ penis video.py
@@ -1,3 +1,7 @@
-import webbrowser
+importimport urllib
 
-open https://www.youtube.com/watch?v=JQz5iGgYETk&list=PLhYNeM90dJSOuDwymGheuSEZGH2u6E7zh&index=3&pp=iAQB
+url = "urllib.request.urlopen(https://www.youtube.com/watch?v=JQz5iGgYETk&list=PLhYNeM90dJSOuDwymGheuSEZGH2u6E7zh&index=3&pp=iAQB
+)"
+
+response = urllib.request.urlopen(https://www.youtube.com/watch?v=JQz5iGgYETk&list=PLhYNeM90dJSOuDwymGheuSEZGH2u6E7zh&index=3&pp=iAQB
+)
